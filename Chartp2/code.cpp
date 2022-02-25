ac3fb86a8bf8577bf05742aaf891772d6b271054
tisuama <1754121135@qq.com>
2022-02-26 03:02:08 +0800

    [Update] Chartp2

--- Chartp2/code.cpp
+++ Chartp2/code.cpp
@@ -1,4 +1,5 @@
 #include <thread>
+#include <chrono>
 #include <iostream>
 
 class background_task {
@@ -10,8 +11,73 @@ public:
 		}
 };
 
-int main() {
+
+struct func {
+	int& i;
+	func(int& i_): i(i_) {}
+	void operator() () {
+		using namespace std::chrono_literals;
+		std::this_thread::sleep_for(2s);
+		for (unsigned j = 0; j < 1000000; j++) {
+			if (j % 100000 == 0) {
+				printf("j: %d, i: %d\n", j, i);
+			}
+		}
+	}
+};
+
+class thread_guard {
+	std::thread& t;
+public:
+		explicit thread_guard(std::thread& t_): t(t_) {}
+		~thread_guard() {
+			if (t.joinable()) {
+				t.join();
+			}
+		}
+		thread_guard(thread_guard const&) = delete;
+		thread_guard& operator=(thread_guard const&) = delete;
+};
+
+void f() {
+	int state = 0;
+	func my_func(state);
+	std::thread t(my_func);
+	try {
+		printf("in current thread: %lu\n", std::this_thread::get_id());
+	} catch(...) {
+		t.join();
+		throw;
+	}
+	t.join();
+}
+
+void f1() {
+	int state = 0;
+	func my_func(state);
+	std::thread t(my_func);
+	thread_guard g(t);
+	printf("thread %lu exit now\n", std::this_thread::get_id());
+}
+
+void test1() {
 	background_task f;
 	std::thread my_thread(f);
-	std::thread my_thread1(background_task());
+	
+	// std::thread my_thread1(background_task());
+	std::thread my_thread2((background_task()));
+}
+
+void test2() {
+	int state = 0;
+	func my_func(state);
+	std::thread my_thread(my_func);
+	my_thread.detach();
+}
+
+int main() {
+	// test1();
+	// test2();
+	// f();
+	f1();
 }
